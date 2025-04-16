50eeb7cbae5b62da57ee826451e4cf1f839d1374
foysal41 <mdfoysal.mf33@gmail.com>
2025-04-16 17:18:24 +0600

    Done welcome with condition

---
+++ welcome-with-condition/welcome-with-condition.c
@@ -0,0 +1,34 @@
+
+
+
+#include <stdio.h>
+
+int main(){
+
+    int A;
+    int B;
+
+    scanf("%d%d" , &A,&B);
+
+    if(A>=B){
+        printf("Yes");
+
+    }else{
+        printf("No");
+    }
+
+    return 0;
+}
+
+
+/*
+Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No"
+
+Input
+10 9
+Output
+Yes
+
+
+https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+*/
