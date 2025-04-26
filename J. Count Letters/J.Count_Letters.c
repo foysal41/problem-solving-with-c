a8f7a1128cfe783ed70533519dff5d450423641c
foysal41 <mdfoysal.mf33@gmail.com>
2025-04-26 12:32:34 +0600

    Done Count string words

---
+++ J. Count Letters/J.Count_Letters.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+        char s[100001];
+        scanf("%s" , &s);
+
+        int freq[26] = {0};
+        int length = strlen(s);
+
+       
+
+        for(int i=0; i<length; i++){
+            int idx = s[i] - 97;
+            
+            freq[idx]++;
+
+        }   
+
+       for(int i=0; i<26; i++){
+            if(freq[i] > 0){
+                printf("%c : %d\n" , i+97, freq[i]);
+            }
+            
+       }
+
+    return 0;
+}
+
+
+/* 
+https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/J
+
+*/
