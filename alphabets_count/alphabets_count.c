b01bc5761c07453badf2b53819d0b00cb91c427d
foysal41 <mdfoysal.mf33@gmail.com>
2025-04-27 09:32:59 +0600

    Done alphabets_count problem

---
+++ alphabets_count/alphabets_count.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+        char s[10001];
+        scanf("%s" , s);
+
+        int freq[26] = {0};
+        int len = strlen(s);
+
+        for(int i=0; i < len; i++){
+            int idx = s[i] - 97;
+            freq[idx]++;
+        }
+
+        for(int i=0; i<26; i++){
+
+            if(freq[i] > 0){
+                printf("%c - %d\n", i+97 , freq[i] );
+            }
+            
+        }
+    return 0;
+}
